933518b935941dc685355eccbd0256e186205fb5
popsoft01 <popoolatunde52@gmail.com>
2021-12-12 11:24:46 +0100

    hospital management system first push wth the basic configuration

--- src/main/java/com/hospitalmanagement/hospital_System/controller/DoctorController.java
+++ src/main/java/com/hospitalmanagement/hospital_System/controller/DoctorController.java
@@ -1,12 +1,27 @@
 package com.hospitalmanagement.hospital_System.controller;
 
+import com.hospitalmanagement.hospital_System.entity.Doctor;
 import com.hospitalmanagement.hospital_System.services.doctor.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+@RestController
+@RequestMapping("/")
 public class DoctorController {
 
     @Autowired
     private DoctorService doctorService;
 
+    @GetMapping("/doctor")
+    public List<Doctor> showAllDoctor(){
+        return doctorService.getAllDoctor();
+    }
+
+    @PostMapping
+    public void addDoctor(@RequestBody Doctor doctor){
+        doctorService.addDoctor(doctor);
+    }
 
 }
